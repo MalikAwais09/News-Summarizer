add6dc1b567fb0959826d1813285f0c7fe6f5ce0
MalikAwais09 <awaisattari047@gmail.com>
2026-01-28 12:53:14 +0500

    Initial Commit

---
+++ News_Summarizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  News_Summarizer
+//
+//  Created by Malik Awais on 28/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ News_Summarizer/News_SummarizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_SummarizerApp.swift
+//  News_Summarizer
+//
+//  Created by Malik Awais on 28/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct News_SummarizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
